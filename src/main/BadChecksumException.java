b7d6b4f3f4ab2074d97af50f5c3a4ce101f008d2
Jack O'Brien <obrienjo@mail.gvsu.edu>
2014-12-01 12:53:10 -0500

    Created BadChecksumException

---
+++ src/main/BadChecksumException.java
@@ -0,0 +1,26 @@
+package main;
+
+public class BadChecksumException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	int expected, received;
+	
+	public BadChecksumException(int expected, int received) {
+		this.expected = expected;
+		this.received = received;
+	}
+	
+	@Override
+	public String getMessage() {
+		return "Bad Checksum. Expected: " + expected + " got: " + received;
+	}
+	
+	public int getExpected() {
+		return expected;
+	}
+	
+	public int getReceived() {
+		return received;
+	}
+
+}
